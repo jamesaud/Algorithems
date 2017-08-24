03154e3dee8fc791c696d7991cf7e3b2cf15fbf6
jamesaud <jamesaud@gmail.com>
2017-08-24 15:42:25 -0400

    Create sort-stack.py

---
+++ sort-stack.py
@@ -0,0 +1,34 @@
+
+
+def sort_stack(stack):
+    if not stack: return stack
+    temp_stack = []
+    sorted_stack = [stack.pop()]
+
+    while stack:
+        val = stack.pop()
+
+        if val >= sorted_stack[-1]:
+            sorted_stack.append(val)
+
+        else:
+            while sorted_stack:
+                if val >= sorted_stack[-1]:
+                    sorted_stack.append(val)
+                    break
+                else:
+                    temp_stack.append(sorted_stack.pop())
+
+            else:
+                sorted_stack.append(val)
+
+            while temp_stack:
+                sorted_stack.append(temp_stack.pop())
+
+    return sorted_stack
+
+
+stack = [1,2 ,5, 3, 6, 3, 8, 43, 3, 2, 33, 44 , 7, 44]
+
+print(sort_stack(stack))
+
